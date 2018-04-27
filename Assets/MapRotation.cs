e31d13fcce1df57ca07eb85c9e17a19da2ff1f98
supernova09 <heeseong98@naver.com>
2018-04-27 20:28:24 +0900

    initial commit  - player movement

---
+++ Assets/MapRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRotation : MonoBehaviour {
+    private Vector3 collisionPoint;
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log(collision.gameObject.name);
+        collisionPoint = collision.gameObject.transform.position;
+    }
+
+
+    void Start () {
+	}
+	
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.RotateAround(collisionPoint, Vector3.up, 5 * moveHorizontal);
+        Debug.Log(collisionPoint);
+    }
+
+    public void setCollisionPoint(Vector3 Point) {
+        collisionPoint = Point;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    MapRotation map;
+    
+    private void OnCollisionEnter(Collision collision) {
+        Vector3 up = new Vector3(0, 500, 0);
+        rb.AddForce(up);
+
+        map.setCollisionPoint(transform.position);
+    }
+
+    void FixedUpdate () {
+	}
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.right * 100);
+
+        map = GameObject.Find("Tiles").GetComponent<MapRotation>();
+    }
+}
