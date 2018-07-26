577a6f7b4dabe09a0849c91041f037dca81f3f2e
supernova09 <heeseong98@naver.com>
2018-07-27 06:20:32 +0900

    implemented scenes added title/main/playing/result scenes

---
+++ Assets/GamePlay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GamePlay : MonoBehaviour {
+    private Transform player;
+    private GameObject map;
+    private CameraController Camera;
+    private Rigidbody rb;
+    
+    // Use this for initialization
+    void Start() {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        Camera = GameObject.Find("Main Camera").GetComponent<CameraController>();
+        rb = player.GetComponent<Rigidbody>();
+        map = GameObject.Find("Map");
+        Camera.SetCameraToPlayer();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (player.position.y < -5)
+            GameOver();
+    }
+    
+    public void ClearGame() {
+        SceneManager.LoadScene(3);
+    }
+
+    private void GameOver() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     private MapRotation map;
+    private GamePlay gp;
     private bool inCollision;
     private int count;
+
+    public GameObject Destination;
+
     private void OnCollisionEnter(Collision collision) {
         Vector3 up = new Vector3(0, 500, 0);
         if (inCollision)
@@ -14,7 +18,11 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce(up);
         inCollision = true;
         map.setCollisionPoint(transform.position);
-        
+
+        if(IsDestination(collision)) {
+            gp.ClearGame();
+        }
+        Debug.Log(collision.gameObject.name);
         Color tileColor = collision.gameObject.GetComponent<Transform>().GetChild(0).GetComponent<Renderer>().material.GetColor("_Color");
         Color playerColor = transform.GetComponent<Renderer>().material.GetColor("_Color");
         transform.GetComponent<Renderer>().material.SetColor("_Color",addColor(tileColor, playerColor));
@@ -36,7 +44,8 @@ public class PlayerController : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(transform.right * 300);
         map = GameObject.Find("Map").GetComponent<MapRotation>();
-
+        gp = GameObject.Find("GamePlay").GetComponent<GamePlay>();
+        SetDestination();
 		Reset();
     }
 
@@ -44,6 +53,14 @@ public class PlayerController : MonoBehaviour {
 		transform.position = new Vector3(0, 5, 0);
 		transform.GetComponent<Renderer>().material.SetColor("_Color", new Color(1.0f, 1.0f, 1.0f));
 	}
+
+    private void SetDestination() {
+        Destination = GameObject.Find("Area_2").transform.GetChild(0).gameObject;
+    }
+
+    private bool IsDestination(Collision c) {
+        return c.transform.parent.name.Equals("Area_1") && c.transform.name.Equals("tile");
+    }
 
 	private Color addColor(Color tile, Color player) {
         Color c = player;
